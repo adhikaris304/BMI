66226cf512802b9aee7e935e48a6bc3eb419418d
Suman Adhikari <adhikaris304@gmail.com>
2019-04-11 14:29:19 +0545

    Complete project

--- app/src/main/java/com/example/bmi/Calculation.java
+++ app/src/main/java/com/example/bmi/Calculation.java
@@ -1,6 +1,30 @@
 package com.example.bmi;
 
 public class Calculation {
-    private float BMI;
+    public float height, weight;
 
+    public Calculation(float height, float weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float arithmetic() {
+        return (this.weight/((this.height/100)*(this.height/100)));
+    }
 }
--- app/src/main/java/com/example/bmi/MainActivity.java
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -2,35 +2,64 @@ package com.example.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity  {
 
-    Button calculate;
-    TextView txtheight, txtweight;
-    EditText etheight, etweight;
+    private EditText etheight, etweight;
+    private TextView tvbmi;
+    private Button btncalculate;
 
-    private Button getCalculate;
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etheight = findViewById(R.id.etheight);
+        etweight = findViewById(R.id.etweight);
+        tvbmi = findViewById(R.id.tvbmi);
+        btncalculate = findViewById(R.id.btncalculate);
 
-        calculate=findViewById(R.id.btncalculate);
-        txtheight=findViewById(R.id.txtheight);
-        txtweight=findViewById(R.id.etweight);
-        etheight=findViewById(R.id.etheight);
-        etweight=findViewById(R.id.etweight);
+        btncalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isEmpty()){
+                    float height = Float.parseFloat(etheight.getText().toString());
+                    float weight = Float.parseFloat(etweight.getText().toString());
+                    Calculation bmi = new Calculation(height, weight);
+                    float arithmetic = bmi.arithmetic();
+                    tvbmi.setText(Float.toString(arithmetic));
+                    float value = Float.parseFloat(tvbmi.getText().toString());
+                    if (value < 18.5) {
+                        Toast.makeText(MainActivity.this, "Under Weight", Toast.LENGTH_LONG).show();
+                    } else if (value > 18.5 && value < 24.9) {
+                        Toast.makeText(MainActivity.this, "Normal Weight", Toast.LENGTH_LONG).show();
+                    } else if (value > 24.9 && value < 29.9) {
+                        Toast.makeText(MainActivity.this, "Over Weight", Toast.LENGTH_LONG).show();
 
+                    } else {
+                        Toast.makeText(MainActivity.this, "Obesity", Toast.LENGTH_LONG).show();
+
+                    }
+                }}
+
+
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-
-
+    public boolean isEmpty(){
+        if (TextUtils.isEmpty((etheight.getText().toString()))){
+            etheight.setError(("Please enter height"));
+            etheight.requestFocus();
+            return true;
+        } else if (TextUtils.isEmpty((etweight.getText().toString()))) {
+            etweight.setError("Please enter weight");
+            etweight.requestFocus();
+            return true;
+        }else return false;
+        }
     }
-}
